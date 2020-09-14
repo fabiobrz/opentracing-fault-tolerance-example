37f73ec96c7b2c22fbb70883eb37b0f6df0f9889
Fabio Burzigotti <fburzigo@redhat.com>
2020-09-14 21:37:48 +0200

    first commit

---
+++ src/main/java/org/example/client/app/HelloResource.java
@@ -0,0 +1,26 @@
+package org.example.client.app;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.concurrent.CompletionStage;
+
+@Path("/")
+public class HelloResource {
+    @Inject
+    private MyService myService;
+
+    @GET
+    @Path("/hello")
+    public String get() {
+        myService.reset();
+        return myService.hello();
+    }
+
+    @GET
+    @Path("/helloAsync")
+    public CompletionStage<String> getAsync() {
+        myService.reset();
+        return myService.helloAsync();
+    }
+}
